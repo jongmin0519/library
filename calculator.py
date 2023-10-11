080f37ae4ee0168729cf69272f95a43edec9d0a6
jongmin0519 <shugomuneto@gmail.com>
2023-10-11 14:46:27 +0900

    사칙연산을 추가하였습니다

---
+++ calculator.py
@@ -0,0 +1,32 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "0으로 나눌 수 없습니다."
+    return x / y
+
+def main():
+    x = float(input("첫 번째 숫자를 입력하세요: "))
+    y = float(input("두 번째 숫자를 입력하세요: "))
+    operation = input("연산을 선택하세요 (+, -, *, /): ")
+
+    if operation == "+":
+        print(f"{x} + {y} = {add(x, y)}")
+    elif operation == "-":
+        print(f"{x} - {y} = {subtract(x, y)}")
+    elif operation == "*":
+        print(f"{x} * {y} = {multiply(x, y)}")
+    elif operation == "/":
+        print(f"{x} / {y} = {divide(x, y)}")
+    else:
+        print("올바른 연산자를 선택하세요.")
+
+if __name__ == "__main__":
+    main()
